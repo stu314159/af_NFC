d61e7005e943500eb047f444dad1233bc9a226a2
Stu Blair <stu314159@gmail.com>
2021-06-15 08:25:22 -0400

    minimum framework added

---
+++ src/afNFC.cu
@@ -0,0 +1,16 @@
+#include <arrayfire.h>
+#include <af/cuda.h>
+
+//CUDA includes
+#include <cuda_runtime.h>
+
+using namespace af;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  af::info();
+  
+  
+  return 0;
+}
